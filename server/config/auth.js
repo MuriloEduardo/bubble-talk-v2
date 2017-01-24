861db50eb32e5d5e7a8553e89bb5376d40f958e7
Murilo Eduardo dos Santos <muriloeduardoooooo@gmail.com>
2017-01-24 01:52:20 +0000

    Teste

--- server/config/auth.js
+++ server/config/auth.js
@@ -2,11 +2,11 @@ module.exports = {
 	'facebookAuth': {
 		'clientID': '1364340600304232',
 		'clientSecret': '1433d45acb25cab95512890476c764b8',
-		'callbackURL': 'http://localhost:8080/auth/facebook/callback'
+		'callbackURL': 'https://bubble-talk-v2-muriloeduardo.c9users.io/auth/facebook/callback'
 	},
 	'googleAuth': {
 		'clientID': '843048291160-9scpmi3qaaojeronpio5gtmre7hrf2rk.apps.googleusercontent.com',
 		'clientSecret': '6afT_0acfNIIqqkCV9oUpwa4',
-		'callbackURL': 'http://localhost:8080/auth/google/callback'
+		'callbackURL': 'https://bubble-talk-v2-muriloeduardo.c9users.io/auth/google/callback'
 	}
 }
